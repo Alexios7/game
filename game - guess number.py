f13f1b6d88ee0142dd5098b04c31845b76a1e1c6
Alexios <118389965+Alexios7@users.noreply.github.com>
2023-06-16 01:33:26 +0300

    Add files via upload

---
+++ game - guess number.py
@@ -0,0 +1,22 @@
+# Hello, welcome. I hope these codes will help you a lot and you will enjoy this program
+
+#                                      Alexios
+
+import random
+number_system = random.randint(1,100)
+number_system = int(number_system)
+print(number_system)
+
+while True :
+    gess = input("is number bigger or smaller?")
+    if gess == "bigger":
+       number_system2 = random.randint(number_system ,100 )
+       print(number_system2) 
+
+    elif gess == "smaller":
+        number_system3 = random.randint(1 , number_system)
+        print(number_system3)
+
+    elif gess == "ok thats right":
+        print("wooooow im amazing")
+        break
